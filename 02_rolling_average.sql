a4ec250079755d0d5933628f6bce63ffd930f62b
Mohammad Zeshaan Shahid <zeshaanshahid2k@gmail.com>
2026-03-16 11:48:07 +0000

    Added rolling average window function for wind speed

---
+++ 02_rolling_average.sql
@@ -0,0 +1,10 @@
+SELECT 
+    date_time,
+    wind_speed_ms,
+    ROUND(AVG(wind_speed_ms) OVER (
+        ORDER BY date_time 
+        ROWS BETWEEN 5 PRECEDING AND CURRENT ROW
+    )::numeric, 2) AS rolling_avg_wind_speed
+FROM t1
+WHERE wind_speed_ms > 0
+LIMIT 20;
